092a8dc0742662338de2e85d01523cb95a469495
copsman <37508611+copsman@users.noreply.github.com>
2022-09-01 16:34:23 +0400

    initial post api for autosave

--- pages/api/autosave.ts
+++ pages/api/autosave.ts
@@ -6,10 +6,23 @@ import { promises as fs } from 'fs';
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse) {
-    //Find the absolute path of the json directory
-    const jsonDirectory = path.join(process.cwd(), 'data');
-    //Read the json data file data.json
-    const fileContents = await fs.readFile(jsonDirectory + '/autosave.json', 'utf8');
-    //Return the content of the data file in json format
-    res.status(200).json(JSON.parse(fileContents));
+    if (req.method === "GET") {
+
+
+        //Find the absolute path of the json directory
+        const jsonDirectory = path.join(process.cwd(), 'data');
+        //Read the json data file data.json
+        const fileContents = await fs.readFile(jsonDirectory + '/autosave.json', 'utf8');
+        //Return the content of the data file in json format
+        res.status(200).json(JSON.parse(fileContents));
+    }
+
+    else if (req.method === "POST"){
+        //Find the absolute path of the json directory
+        const jsonDirectory = path.join(process.cwd(), 'data');
+        //Read the json data file data.json
+        const fileContents = await fs.writeFile(jsonDirectory + '/test.json', '{"message":"Hello!"}', 'utf8');
+        //Return the content of the data file in json format
+        res.status(200).json({message: "POST!"});
+    }
 }
